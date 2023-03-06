a723b47bcbd4d57e56df05eb86cbde82c19e70c5
Theo Holmvik <theo.holmvik@core.sec>
2023-03-06 10:14:47 +0100

    dfs

---
+++ 4_AddDfs.ps1
@@ -0,0 +1,12 @@
+Install-WindowsFeature -Name FS-DFS-Namespace,FS-DFS-Replication,RSAT-DFS-Mgmt-Con `
+                        -ComputerName srv1 `
+                        -IncludeManagementTools
+
+Invoke-Command -ComputerName srv1 -ScriptBlock {New-Item -Path "c:\" -Name 'dfsroots' -ItemType "directory"}
+Invoke-Command -ComputerName srv1 -ScriptBlock {New-Item -Path "c:\" -Name 'shares' -ItemType "directory"}
+
+Enter-PSSession -ComputerName srv1
+
+$folders = ('C:\dfsroots\files','C:\shares\accounting','C:\shares\hr','C:\shares\it','C:\shares\legal', 'C:\shares\inactive')
+
+$folders | ForEach-Object {$sharename = (Get-Item $_).name; New-SMBShare -Name $shareName -Path $_ -FullAccess Everyone}
