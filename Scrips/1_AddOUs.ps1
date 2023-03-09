9fbcd633412180cfa0bee6900b7acba5e403f588
dybe5 <97792696+dybe5@users.noreply.github.com>
2023-03-09 11:04:21 +0100

    Update 1_AddOUs.ps1

--- Scrips/1_AddOUs.ps1
+++ Scrips/1_AddOUs.ps1
@@ -16,27 +16,3 @@ foreach ($ou in $topOUs) {
                         -ProtectedFromAccidentalDeletion:$false #True før livemiljø
         }
 }
-
-# ----- Gruppe Struktur ----- #
-
-foreach ($department in $departments) {
-    $path = Get-ADOrganizationalUnit -Filter * | 
-            Where-Object {($_.name -eq "$department") `
-            -and ($_.DistinguishedName -like "OU=$department,OU=$security_groups,*")}
-    New-ADGroup -Name "g_$department" `
-            -SamAccountName "g_$department" `
-            -GroupCategory Security `
-            -GroupScope Global `
-            -DisplayName "g_$department" `
-            -Path $path.DistinguishedName `
-            -Description "$department group"
-}
-
-#Lager en global gruppe
-New-ADGroup -name "g_all_employee" `
-            -SamAccountName "g_all_employee" `
-            -GroupCategory Security `
-            -GroupScope Global `
-            -DisplayName "g_all_employee" `
-            -path "OU=Security_Groups,DC=core,DC=sec" `
-            -Description "all employee"
