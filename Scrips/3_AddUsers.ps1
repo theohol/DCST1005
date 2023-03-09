19b195aded2432c160b163a5a4c60303a2ed5ac4
andefjer <126465791+andefjer@users.noreply.github.com>
2023-03-09 11:10:54 +0100

    Update 3_AddUsers.ps1

--- Scrips/3_AddUsers.ps1
+++ Scrips/3_AddUsers.ps1
@@ -1,19 +1,8 @@
-$users = Import-Csv -Path 'C:\Users\theo.holmvik\DCST1005-oblig\DCST1005\midlertidigBrukere.csv' -Delimiter ";"
 
-Function New-UserPassword {
-    $chars = [char[]](
-        (33..43 | ForEach-Object {[char]$_}) + #DEC 44 er "," og derfor hoppes den over
-        (45..47 | ForEach-Object {[char]$_}) +
-        (58..64 | ForEach-Object {[char]$_}) +
-        (91..96 | ForEach-Object {[char]$_}) +
-        (123..126 | ForEach-Object {[char]$_}) +
-        (48..57 | ForEach-Object {[char]$_}) +
-        (65..90 | ForEach-Object {[char]$_}) +
-        (97..122 | ForEach-Object {[char]$_})
-    )
 
-    -join (0..14 | ForEach-Object { $chars | Get-Random })
-}
+#formatering av csv fil
+$users = Import-Csv -Path 'C:\Users\anders.fjermedal\Documents\DCST1005\midlertidigBrukere.csv' -Delimiter ";"
+
 
 function New-UserInfo {
     param (
@@ -31,86 +20,104 @@ function New-UserInfo {
     }
 
     $UserPrincipalName = $("$($fornavn).$($etternavn)").ToLower()
-    $UserPrincipalName = $UserPrincipalName.Replace('æ','e')
-    $UserPrincipalName = $UserPrincipalName.Replace('ø','o')
-    $UserPrincipalName = $UserPrincipalName.Replace('å','a')
-    $UserPrincipalName = $UserPrincipalName.Replace('é','e')
-
+    $UserPrincipalName = $UserPrincipalName.Replace('æ','e').Replace('ø','o').Replace('å','a').Replace('é','e')
+    
     Return $UserPrincipalName
 }
 
+
+
 $csvfile = @()
-$exportpath = 'C:\Users\theo.holmvik\DCST1005-oblig\DCST1005\brukere.csv'
-$finalexport = 'C:\Users\theo.holmvik\DCST1005-oblig\DCST1005\faktiskeBrukere'
+
+$exportpath = 'C:\Users\anders.fjermedal\Documents\DCST1005\brukere.csv'
+
+$samcheck = @()
+
+$123 = Get-ADUser -Filter * -Properties SamAccountName | Select-Object -ExpandProperty SamAccountName   #tatt fra chatgpt
+foreach ($user in $123){
+    $samcheck += $user.Trim()
+}
+
 
 foreach ($user in $users) {
-    $password = New-UserPassword
+    $pass1 = (33..122-as [char[]] | Where-Object {($_ -ne 59)} )
+    $password = -join (0..14 | ForEach-Object { $pass1 | Get-Random })
+    
     $line = New-Object -TypeName psobject
+    
+    $sam = (New-UserInfo -Fornavn $user.GivenName -Etternavn $user.SurName)
+    
+        if ($sam.Length -gt 19) {
+            $sam = $sam.Substring(0, 18) 
+        }
+        $samcheck += $sam
+        while ($sam -in $samcheck ) {
+            $sam = "1" + $sam  
+        } 
+        $samcheck += $sam
+        if ($sam.Length -gt 19) {
+            $sam = $sam.Substring(0, 18) 
+        }
+        $samcheck += $sam
+        if( $sam.Substring($sam.Length - 1) -eq "."){
+            $sam = $sam.Substring(0, $sam.Length - 1)
+        }
+        $sam
+        $samcheck += $sam
+        [string] $samaccountname = $sam
+        
+        [string] $department = $user.Department
+        [string] $searchdn = "OU=$department,OU=$security_users,*"
+        $path = Get-ADOrganizationalUnit -Filter * | Where-Object {($_.name -eq $user.Department) -and ($_.DistinguishedName -like $searchdn)}
+        
 
-    Add-Member -InputObject $line -MemberType NoteProperty -Name GivenName -Value $User.GivenName
-    Add-Member -InputObject $line -MemberType NoteProperty -Name SurName -Value $user.SurName
-    Add-Member -InputObject $line -MemberType NoteProperty -Name UserPrincipalName -Value "$(New-UserInfo -Fornavn $user.GivenName -Etternavn $user.SurName)@core.sec"
-    Add-Member -InputObject $line -MemberType NoteProperty -Name DisplayName -Value "$($user.GivenName) $($user.SurName)" 
-    Add-Member -InputObject $line -MemberType NoteProperty -Name department -Value $user.Department
-    Add-Member -InputObject $line -MemberType NoteProperty -Name Password -Value $password
-    Add-Member -InputObject $line -MemberType NoteProperty -Name Path -Value # Get-ADOrganizationalUnit -Filter * | Where-Object {($_.name -eq $user.Department) -and ($_.DistinguishedName -like $searchdn)}
-    # Skrive pathen inn direkte eller legge den til etter at brukerne er opprettet? 
+        
+        Add-Member -InputObject $line -MemberType NoteProperty -Name GivenName -Value $user.GivenName `
+          -PassThru | Add-Member -MemberType NoteProperty -Name SurName -Value $user.SurName `
+          -PassThru | Add-Member -MemberType NoteProperty -Name UserPrincipalName -Value "$sam@core.sec" `
+          -PassThru | Add-Member -MemberType NoteProperty -Name DisplayName -Value "$($user.GivenName) $($user.SurName)" `
+          -PassThru | Add-Member -MemberType NoteProperty -Name department -Value $user.Department `
+          -PassThru | Add-Member -MemberType NoteProperty -Name Password -Value $password `
+          -PassThru | Add-Member -MemberType NoteProperty -Name Path -Value $path `
+          -PassThru | Add-Member -MemberType NoteProperty -Name SamAccountName -Value $samaccountname
+  
+  
     $csvfile += $line
 }
 
-$csvfile | Export-Csv -Path $exportpathfinal -NoTypeInformation -Encoding 'UTF8'
-Import-Csv -Path $exportpathfinal | ConvertTo-Csv -NoTypeInformation | ForEach-Object { $_ -Replace '"', ""} | Out-File $finalexport -Encoding 'UTF8'
+$csvfile | Export-Csv -Path $exportpath -Delimiter ";" -Usequotes Never -NoTypeInformation -Encoding 'UTF8'
 
+#plassere brukere i OU
 
-
-
-$users = Import-Csv -path 'C:\Users\theo.holmvik\DCST1005-oblig\DCST1005\faktiskeBrukere.csv' -Delimiter ","
+$users = Import-Csv -path 'C:\Users\anders.fjermedal\Documents\DCST1005\faktiskeBrukere.csv' -Delimiter ";"
 
 foreach ($user in $users) {
-        $sam = $user.UserPrincipalName.Split("@")
-        if ($sam[0].Length -gt 19) {
-            "SAM for lang, bruker de 19 første tegnene i variabelen"
-            $sam[0] = $sam[0].Substring(0, 19) 
-        }
-        $sam[0]
-
-        [string] $samaccountname = $sam[0]
-
-        [string] $department = $user.Department
-        [string] $searchdn = "OU=$department,OU=$security_users,*"
-        # $path = Get-ADOrganizationalUnit -Filter * | Where-Object {($_.name -eq $user.Department) -and ($_.DistinguishedName -like $searchdn)}????
-        
-        if (!(Get-ADUser -Filter "sAMAccountName -eq '$($samaccountname)'")) {
-            Write-Host "$samaccountname does not exist." -ForegroundColor Green
-        
-            Write-Host "Creating User ....%" -ForegroundColor Green
-            Write-Host $user.DisplayName -ForegroundColor Green
 
             New-ADUser `
-            -SamAccountName $samaccountname `
+            -SamAccountName $user.samaccountname `
             -UserPrincipalName $user.UserPrincipalName `
-            -Name $user.DisplayName `
+            -Name $user.SamAccountName `
             -GivenName $user.GivenName `
             -Surname $user.SurName `
             -Enabled $True `
             -ChangePasswordAtLogon $false `
-            -DisplayName $user.DisplayName `
+            -DisplayName $user.DisplayName`
             -Department $user.Department `
-            -Path $user.path ` #må være i csv fil
-            -AccountPassword (convertto-securestring $user.Password -AsPlainText -Force)
+            -Path $user.path `
+            -AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -Force)
 
         }
-    }
+    
 
 
 $ADUsers = @()
-
+$departments = @('management','accounting','it','hr','legal','inactive')
 foreach ($department in $departments) {
     $ADUsers = Get-ADUser -Filter {Department -eq $department} -Properties Department
-    #Write-Host "$ADUsers som er funnet under $department"
 
     foreach ($aduser in $ADUsers) {
         Add-ADPrincipalGroupMembership -Identity $aduser.SamAccountName -MemberOf "g_$department"
     }
 
 }
+
