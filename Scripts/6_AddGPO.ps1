1edd715699d20bcdeabd3da0105be19f18654974
dybe5 <97792696+dybe5@users.noreply.github.com>
2023-03-10 15:30:39 +0100

    Update 6_AddGPO.ps1

--- Scripts/6_AddGPO.ps1
+++ Scripts/6_AddGPO.ps1
@@ -1,21 +1,15 @@
 Import-Module GroupPolicy 
 
-# Må bli enige om hvilke departments som skal ha hvilke policies.
-
-## Eksempel domene
-$ou = 'LearnIT_Users'
-$domainDn = (Get-ADDomain).DistinguishedName
-$ouDn = "OU=$ou,$domainDn"
-
-## KAN VÆRE 'Set-GPregistryValue'-DELEN MÅ FJERNES ##
-
 ## Prohibit access to Control Panel and PC settings
 $gpoName = 'No Control Panel and PC settings'
 New-GPO -Name $gpoName -comment 'Prohibit access to Control Panel and PC settings'
 
 Set-GPRegistryValue -Name $gpoName -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName Nocontrolpanel -Type DWord -Value 01
 
-New-GPLink -Name $gpoName -Target $ouDn -LinkEnabled 'Yes'
+$OU = 'OU=accounting', 'OU=hr', 'OU=legal', 'OU=inactive'
+foreach ($group in $OU) {
+    Get-GPO -Name $GPOName | New-GPLink -Target "$group,OU=Security_Users,DC=secure,DC=sec"
+}
 
 ## Prevent access to the command prompt
 $gpoName = 'No command prompt'
@@ -23,7 +17,10 @@ New-GPO -Name $gpoName -comment 'Prevent access to the command prompt'
 
 Set-GPRegistryValue -Name $gpoName -Key "HKCU\Software\Policies\Microsoft\Windows\System" -ValueName DisableCMD -Type DWord -Value 01
 
-New-GPLink -Name $gpoName -Target $ouDn -LinkEnabled 'Yes'
+$OU = 'OU=accounting', 'OU=hr', 'OU=legal', 'OU=inactive'
+foreach ($group in $OU) {
+    Get-GPO -Name $GPOName | New-GPLink -Target "$group,OU=Security_Users,DC=secure,DC=sec"
+}
 
 ## Deny all removable storage access
 $gpoName = 'Deny all removable storage access'
@@ -31,7 +28,10 @@ New-GPO -Name $gpoName -comment 'Deny all removable storage access'
 
 Set-GPRegistryValue -Name $gpoName -Key "HKCU\Software\Policies\Microsoft\Windows\RemovableStorageDevices" -ValueName Deny_All -Type DWord -Value 01
 
-New-GPLink -Name $gpoName -Target $ouDn -LinkEnabled 'Yes'
+$OU = 'OU=accounting', 'OU=hr', 'OU=legal', 'OU=inactive'
+foreach ($group in $OU) {
+    Get-GPO -Name $GPOName | New-GPLink -Target "$group,OU=Security_Users,DC=secure,DC=sec"
+}
 
 ## Prohibit users from installing unwanted software
 $gpoName = 'No installing of unwanted software'
@@ -39,7 +39,10 @@ $gpoName = 'No installing of unwanted software'
 New-GPO -Name $gpoName -comment 'Prohibit users from installing unwanted software'
 Set-GPRegistryValue -Name $gpoName -Key "HKLM\Software\Policies\Microsoft\Windows\Installer" -ValueName DisableUserInstalls -Type DWord -Value 01
 
-New-GPLink -Name $gpoName -Target $ouDn -LinkEnabled 'Yes'
+$OU = 'OU=accounting', 'OU=hr', 'OU=legal', 'OU=inactive'
+foreach ($group in $OU) {
+    Get-GPO -Name $GPOName | New-GPLink -Target "$group,OU=Security_Users,DC=secure,DC=sec"
+}
 
 ## Prevent auto-restarts with logged on users during scheduled update installations
 $gpoName = 'No auto-restarts'
@@ -47,8 +50,7 @@ New-GPO -Name $gpoName -comment 'Prevent auto-restarts with logged on users duri
 
 Set-GPRegistryValue -Name $gpoName -Key "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ValueName NoAutoRebootWithLoggedOnUsers -Type Dword -Value 01
 
-New-GPLink -Name $gpoName -Target $ouDn -LinkEnabled 'Yes'
-
-
-
-
+$OU = 'Security_Users'
+foreach ($group in $OU) {
+    Get-GPO -Name $GPOName | New-GPLink -Target "$group,DC=secure,DC=sec"
+}
