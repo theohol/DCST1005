53020a8e9aa00a128ae5211bfd0422d077bb978c
trygvmk <trygvmk@stud.ntnu.no>
2023-03-13 09:59:13 +0100

    Update 7_MoveComputers.ps1

--- Scripts/7_MoveComputers.ps1
+++ Scripts/7_MoveComputers.ps1
@@ -1,14 +1,3 @@
-#Flytting av PCer ved hjelp av UI. Tror ikkje vi kommer til å bruke denne men kan være kjekt å ha
-
-#$ADComps= Get-ADComputer -Filter * -SearchBase "CN=Computers,DC=secure,dc=sec"| Select-Object -Property Name |sort -Property name | Out-GridView -PassThru –title “Select Computers to Move”| Select -ExpandProperty Name
-#$ADOUs= Get-ADOrganizationalUnit -Filter * | Select-Object -Property DistinguishedName | Out-GridView -PassThru –title “Select Target OU”| Select-Object -ExpandProperty DistinguishedName
-#Foreach($ou in $ADOUs){
-#   Foreach($comp in $ADComps){
-#       get-adcomputer $comp |Move-ADObject -TargetPath "$ou" -Verbose -PassThru 
-#   }
-#}
-
-
 #Flytting av PCer ved hjelp av csv fil
 
 $OUPCer = Import-Csv -Path 'C:\DCST1005\CSVFiler\MaskinerTilOU.csv' -Delimiter ";"
